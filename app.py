26458aaacf5fa152beecbec3633294467bb1268f
ash-perfect <akshay.dn523@gmail.com>
2020-03-10 13:43:45 +0530

    Menu is up and running

--- app.py
+++ app.py
@@ -2,17 +2,15 @@
 
 from flask import Flask,jsonify, request, send_from_directory, url_for
 app = Flask(__name__)
+import pickle
 
 @app.route('/')
 def hello_world():
     return 'Hey, we have Flask in a Docker container!'
-@app.route('/filenames')
-def filenames():
-    fln = [];
-    item = "Budweiser"
-    for i in range(5):
-        fln.append(item)
-    return jsonify(fln)
+@app.route('/menu')
+def fetch_menu():
+    str_menu = pickle.load(open('half_menu_main.pkl','rb'))
+    return str_menu.to_json()
 
 
 @app.route('/order', methods=['POST'])
