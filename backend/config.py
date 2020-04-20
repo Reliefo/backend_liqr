e20ff9189aa1c525d091bac88fe3dccfbeb83bfe
ash-perfect <akshay.dn523@gmail.com>
2020-04-20 20:39:40 +0530

    added auth to mongo

--- backend/config.py
+++ backend/config.py
@@ -4,7 +4,7 @@ if mongo_setup == "docker_local":
     MONGO_DB = "local"
     MONGO_HOST = "mongodb://mongo:27017"
 elif mongo_setup == "aws":
-    MONGO_DB = "local"
+    MONGO_DB = "reliefo"
     MONGO_HOST = "mongodb://ec2-13-232-202-63.ap-south-1.compute.amazonaws.com:27017"
 else:
     MONGO_DB = "local"
