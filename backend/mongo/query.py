8bf2d68894db69818a871c12d73eb9ba7967636c
ash-perfect <akshay.dn523@gmail.com>
2020-04-21 09:45:30 +0530

    adding queued status to all new orders

--- backend/mongo/query.py
+++ backend/mongo/query.py
@@ -52,7 +52,7 @@ def order_placement(input_order):
     table_order = TableOrder(table=str(ordered_table.name), table_id=str(ordered_table.id),
                              timestamp=datetime.now())
     for order in input_order['orders']:
-        food_list = [FoodItemMod.from_json(json_util.dumps(food_dict)) for food_dict in order['food_list']]
+        food_list = [FoodItemMod.from_json(json_util.dumps({**food_dict,**{'status':"queued"}})) for food_dict in order['food_list']]
         table_order.orders.append(
             Order(placed_by=User.objects.get(id=order['placed_by']).to_dbref(), food_list=food_list).save().to_dbref())
         table_order.timestamp = datetime.now()
@@ -60,7 +60,6 @@ def order_placement(input_order):
     ordered_table.update(push__table_orders=table_order.to_dbref())
     Restaurant.objects(tables__in=[str(ordered_table.id)]).update(push__table_orders=table_order.to_dbref())
     return TableOrder.objects.get(id=table_order.id).to_json()
-
 
 def assistance_req(assist_input):
     curr_table = Table.objects.get(id=assist_input['table'])
