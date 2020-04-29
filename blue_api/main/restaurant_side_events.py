60e6e03f1a7e9e41a3be50bf9b3810b54edb59bc
ash-perfect <akshay.dn523@gmail.com>
2020-04-29 19:47:17 +0530

    added timestamp to order updates

--- blue_api/main/restaurant_side_events.py
+++ blue_api/main/restaurant_side_events.py
@@ -40,7 +40,7 @@ def send_new_orders(message):
         order_status_completed(status_tuple)
 
     sending_dict = {'table_order_id': status_tuple[0], 'type': message['type'], 'order_id': status_tuple[1],
-                    'food_id': status_tuple[2], 'kitchen_app_id': message['kitchen_app_id']}
+                    'food_id': status_tuple[2], 'kitchen_app_id': message['kitchen_app_id'], 'timestamp': str(datetime.now())}
 
     if sending_dict['type'] == 'completed':
         push_order_complete_notification(sending_dict)
