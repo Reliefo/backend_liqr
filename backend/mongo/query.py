a8d2044d2906d36763efe8caaafe5d3edfe6c817
ash-perfect <akshay.dn523@gmail.com>
2020-05-03 00:28:56 +0530

    many changes configuring hone screem

--- backend/mongo/query.py
+++ backend/mongo/query.py
@@ -6,7 +6,7 @@ np.random.shuffle(li)
 
 
 def return_user_details(user_id):
-    pass
+    return User.objects.get(id=user_id).to_json()
 
 
 def return_restaurant(rest_id):
@@ -210,6 +210,8 @@ def configuring_restaurant(message):
         return configuring_bar_category(request_type, message)
     elif element_type == 'food_item':
         return configuring_food_item(request_type, message)
+    elif element_type == 'home_screen_tags':
+        return configuring_home_screen(request_type, message)
     else:
         return {'status': 'element not recognized'}
 
@@ -326,6 +328,56 @@ def configuring_food_item(request_type, message):
         this_object = FoodItem.objects.get(id=message['food_id'])
         for field in message['editing_fields'].keys():
             if field == 'food_options':
+                this_object[field] = FoodOptions.from_json(json_util.dumps(message['editing_fields'][field]))
+            else:
+                this_object[field] = message['editing_fields'][field]
+        this_object.save()
+        return message
+    else:
+        return {'status': 'command type not recognized'}
+
+
+def configuring_home_screen(request_type, message):
+    if request_type == 'add':
+        restaurant_ob = Restaurant.objects(restaurant_id=message['restaurant_id']).first()
+        if message['add_to'] == "navigate_better":
+            restaurant_ob.navigate_better_tags.append(message['tag_name'])
+            message['status'] = "added"
+        elif message['add_to'] == "home_screen":
+            restaurant_ob.home_screen_tags.append(message['tag_name'])
+            message['status'] = "added"
+        else:
+            message['status'] = "wrong add to"
+        restaurant_ob.save()
+        return message
+    elif request_type=='attach':
+        FoodItem.objects.get(id=message['food_id']).update(push__tags= message['tag_name'])
+        message['status'] = "Tag "+message['tag_name']+" attached to Food Item"
+        return message
+    elif request_type=='remove':
+        FoodItem.objects.get(id=message['food_id']).update(pull__tags= message['tag_name'])
+        message['status'] = "Tag "+message['tag_name']+" removed from Food Item"
+        return message
+    elif request_type == 'delete':
+        restaurant_ob = Restaurant.objects(restaurant_id=message['restaurant_id']).first()
+        if message['remove_from'] == "navigate_better":
+            restaurant_ob.navigate_better_tags.remove(message['tag_name'])
+            message['status'] = "deleted"
+        elif message['remove_from'] == "home_screen":
+            restaurant_ob.home_screen_tags.remove(message['tag_name'])
+            message['status'] = "deleted"
+        else:
+            message['status'] = "wrong remove from"
+        if message['status'] == 'deleted':
+            for food in FoodItem.objects(tags__in=[message['tag_name']]):
+                food.tags.remove(message['tag_name'])
+                food.save()
+        restaurant_ob.save()
+        return message
+    elif request_type == 'edit':
+        this_object=FoodItem.objects.get(id=message['food_id'])
+        for field in message['editing_fields'].keys():
+            if field== 'food_options':
                 this_object[field] = FoodOptions.from_json(json_util.dumps(message['editing_fields'][field]))
             else:
                 this_object[field] = message['editing_fields'][field]
