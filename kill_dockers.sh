e739b97f5687bccff2093d41acf5876a9d07a138
EC2 Default User <ec2-user@ip-172-31-8-56.ap-south-1.compute.internal>
2020-05-03 17:59:18 +0000

    pull this B

--- kill_dockers.sh
+++ kill_dockers.sh
@@ -1,2 +1,7 @@
+git pull
+cd relief_customer/
+git pull https://github.com/akshay-bizz/relief_customer/ master
+cd ../
+
 sudo docker container kill $(sudo docker ps -q)
 sudo docker-compose up --build
