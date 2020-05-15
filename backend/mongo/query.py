0423fee10cf908f79439bbc570c7c60787b1c09b
akshay-bizz <akshaydn.bizz@gmail.com>
2020-05-15 18:05:54 +0530

    changed tags

--- backend/mongo/query.py
+++ backend/mongo/query.py
@@ -22,14 +22,12 @@ def return_restaurant(rest_id):
 
 
 def home_screen_lists(rest_id):
-    home_screen = {'available_tags': Restaurant.objects(restaurant_id=rest_id).first().home_screen_tags,
-                   'navigate better': {
-                       'available_tags': Restaurant.objects(restaurant_id=rest_id).first().navigate_better_tags}}
-
-    for tag in home_screen['available_tags']:
-        home_screen[tag] = [str(food.id) for food in FoodItem.objects.filter(restaurant_id=rest_id).filter(tags__in=[tag])]
-    for tag in home_screen['navigate better']['available_tags']:
-        home_screen['navigate better'][tag] = [str(food.id) for food in
+    home_screen = {}
+    for tag in Restaurant.objects(restaurant_id=rest_id).first().home_screen_tags:
+        home_screen[tag] = [str(food.id) for food in
+                            FoodItem.objects.filter(restaurant_id=rest_id).filter(tags__in=[tag])]
+    for tag in Restaurant.objects(restaurant_id=rest_id).first().navigate_better_tags:
+        home_screen['navigate_better'][tag] = [str(food.id) for food in
                                                FoodItem.objects.filter(restaurant_id=rest_id).filter(tags__in=[tag])]
 
     return json.dumps(home_screen)
