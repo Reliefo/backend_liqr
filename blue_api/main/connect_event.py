4cc1202afa1ea2e6321d19db89d4a4bdbb339613
akshay-bizz <akshaydn.bizz@gmail.com>
2020-06-21 12:07:04 +0530

    well

--- blue_api/main/connect_event.py
+++ blue_api/main/connect_event.py
@@ -13,13 +13,13 @@ from flask_socketio import emit, join_room, leave_room, disconnect, rooms
 @socket_io.on('connect', namespace=our_namespace)
 # @jwt_required
 def connect():
-    username = get_jwt_identity()
-    app_user = AppUser.objects(username=username).first()
-    previous_sid = app_user.sid
+    # username = get_jwt_identity()
+    app_user = AppUser.objects(username="akshay_hou").first()
+    # previous_sid = app_user.sid
 
-    sys.stderr.write("LiQR_Error: "+username+" who is a "+request.args.get('jwt')+" connected\n")
-    if previous_sid:
-        disconnect(previous_sid)
+    sys.stderr.write("LiQR_Error: "+"username"+" who is a "+str(request.args)+" connected\n")
+    # if previous_sid:
+    #     disconnect(previous_sid)
     if app_user.user_type == "manager":
         join_room(app_user.restaurant_id)
     elif app_user.user_type == "owner":
@@ -36,8 +36,8 @@ def connect():
             sys.stderr.write("LiQR_Error: "+app_user.rest_user.name+" who is a "+app_user.user_type+" joined"+app_user.rest_user.current_table_id+"\n")
         except:
             pass
-    AppUser.objects(username=username).first().update(set__sid=request.sid)
-    sys.stderr.write("LiQR_Error: "+username+" who is a "+app_user.user_type+" connected\n")
+    AppUser.objects(username=app_user.username).first().update(set__sid=request.sid)
+    sys.stderr.write("LiQR_Error: "+app_user.username+" who is a "+app_user.user_type+" connected\n")
 
 
 @socket_io.on('disconnect', namespace=our_namespace)
