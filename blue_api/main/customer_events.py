a473a7ba920c8bc1737be2a7acde120ede9a65d0
akshay-bizz <akshaydn.bizz@gmail.com>
2020-06-21 17:59:03 +0530

    updated

--- blue_api/main/customer_events.py
+++ blue_api/main/customer_events.py
@@ -155,7 +155,8 @@ def fetch_the_bill(message):
         returning_dict.pop('order_history')
         for staff in table.staff:
             if staff.endpoint_arn:
-                push_bill_request_notification(returning_dict, staff.endpoint_arn, staff_id=str(staff.id))
+                returning_dict['staff_id'] = str(staff.id)
+                push_bill_request_notification(returning_dict, staff.endpoint_arn)
     else:
         returning_json = json_util.dumps({'status': "billed", 'table_id': input_dict['table_id'],
                                           'message': 'Your personal bill will be brought to you',
