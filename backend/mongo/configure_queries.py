90374bd1fc92844e2556ea3746a5612457cc0580
akshay-bizz <akshaydn.bizz@gmail.com>
2020-06-28 14:46:54 +0530

    gooofd debug

--- backend/mongo/configure_queries.py
+++ backend/mongo/configure_queries.py
@@ -171,6 +171,7 @@ def configuring_food_item(request_type,message):
         return message
     elif request_type == 'edit':
         this_object=FoodItem.objects.get(id=message['food_id'])
+        sys.stderr.write("LiQR_Error: "+message+" who is a connected\n")
         for field in message['editing_fields'].keys():
             if field=='food_customization':
                 this_object[field] = [FoodCustomization.from_json(json_util.dumps(customization)) for customization in message['editing_fields'][field]]
